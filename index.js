8511ec930f5f95450c913dc39c8a3b7610204e1b
kuu12 <maaail@hotmail.com>
2018-08-19 01:54:36 +0800

    Optimize

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
 var fs = require('fs');
 var p = require('path');
 
-module.exports = function (root, ignores) {
-    ignores = !ignores ? [] :
-        Array.isArray(ignores) ? ignores :
-            [ignores];
+/**
+ * @param   {string}  root      Directory path you want to read.
+ * @param   {Array}   excludes  Exlude rules. A rule can be a regular expression / string / function.
+ * @returns {Array}             Paths of files.
+ */
+module.exports = function (root, excludes) {
+    excludes = [].concat(excludes).filter(Boolean);
 
-    var directories = ['.'];
-    var directory;
-    var files = [];
+    var dirList = ['.'];
+    var dir;
+    var fileList = [];
 
-    while (directories.length) {
-        directory = directories.pop();
-        fs
-            .readdirSync(p.join(root, directory))
-            .filter(filter)
-            .forEach(dispatch);
-    }
+    while (dir = dirList.pop()) fs
+        .readdirSync(p.join(root, dir))
+        .filter(filter)
+        .forEach(dispatch);
 
-    return files;
+    return fileList;
 
     function filter(path) {
-        return !ignores.length || !ignores.some(function (pattern) {
-            return isIgnored(pattern, path);
+        return excludes.every(function (rule) {
+            return !isExclude(rule, path);
         });
     }
 
     function dispatch(name) {
-        var path = p.join(directory, name);
+        var path = p.join(dir, name);
         if (fs
             .statSync(p.join(root, path))
             .isDirectory()
-        ) {
-            directories.push(path);
-        } else {
-            files.push(path);
-        }
+        )
+            dirList.push(path);
+        else
+            fileList.push(path);
     }
 }
 
-function isIgnored(pattern, path) {
-    if (pattern instanceof Function) return pattern(path);
-    if (pattern instanceof RegExp) return pattern.test(path);
-    if (typeof pattern === 'string') return path.indexOf(pattern) !== -1;
+function isExclude(rule, path) {
+    if (rule instanceof Function) return rule(path);
+    if (rule instanceof RegExp) return rule.test(path);
+    if (typeof rule === 'string') return path.indexOf(rule) !== -1;
 
-    console.warn(
-        'Invalid ignore pattern : ' +
-        'expect a function/regular expression/string but found',
-        pattern
+    console.error(
+        'Invalid exclude rule : ' +
+        'expect a regular expression/string/function but found',
+        rule
     );
 }
-
